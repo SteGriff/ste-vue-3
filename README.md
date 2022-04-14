1a23ebdc5967e0447bd2f9e24b8e806c8d4d13d3
Ste Griffiths <github@stegriff.co.uk>
2022-04-14 15:37:25 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# ste-vue-3
+
+Ste's Vue3 starter
+
+## How's it made?
+
+ + Run the Vue3 wizard with `npm init vue@latest`
+ + Remove the cruft
+ + Add tachyons.css into `/public`
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { reactive } from "@vue/reactivity";
+
+const state = reactive({
+  person: 'Gromit'
+})
+</script>
+
+<template>
+  <main class="sans-serif ph2">
+    <h1>Hello {{ state.person }}</h1>
+    <input v-model="state.person">
+  </main>
+</template>
+
